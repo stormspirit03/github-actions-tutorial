53cc478280780361e9be2829f153dc7d6450a62f
Raviraj Gardi <53505985+stormspirit03@users.noreply.github.com>
2024-04-06 11:16:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world"
+print("Hello world")
+
+a= 50 
+
+
+
+
+b = 60
+
+
+c= a+b
+
+print("gibrish long string aweweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeweqwqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqwqewewq")
+
+print(a+b)
